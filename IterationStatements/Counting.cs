b74dc739c664ba1aa1d8f026a83d66e9e20d3424
Nick Carlson <CarlsonNicholas82@gmail.com>
2022-12-01 14:47:55 -0500

    All Main Assignments Done

---
+++ IterationStatements/Counting.cs
@@ -0,0 +1,127 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+namespace IterationStatements
+{
+    public class Counting
+    {
+        //Method that will List all numbers from negative one thousand to positive one thousand
+        public static void CountingNumbers()
+        {
+            int activeNumber = -1000;
+            do
+            {
+                Console.WriteLine(activeNumber);
+                activeNumber++;
+
+            } while (activeNumber < 1001);
+
+        }
+
+        // Method that will count up by 3's from 3 to 999
+        public static void CountingThrees()
+        {
+            int threes = 3;
+            do
+            {
+                Console.WriteLine(threes);
+                threes += 3;
+
+            } while (threes != 1002);
+
+        }
+        // Method to check if two numbers are the same
+        public static void SameNumber()
+        {
+            Console.WriteLine("Please Provide your first number");
+            var numOne = Console.ReadLine();
+
+            Console.WriteLine("Please provide your second number");
+            var numTwo = Console.ReadLine();
+
+            if (numOne == numTwo)
+            {
+                Console.WriteLine($"{numOne} and {numTwo} are the same!");
+            }
+
+            else
+            {
+                Console.WriteLine($"Sorry, {numOne} and {numTwo} are not the same :(");
+            }
+        }
+        // Method to check if a number is even or odd
+        public static void EvenOrOdd()
+        {
+            Console.WriteLine("Write a number! ");
+            int inputEvenOdd = Convert.ToInt32(Console.ReadLine());
+
+            if (inputEvenOdd % 2 == 0)
+            {
+                Console.WriteLine($"{inputEvenOdd} is even!");
+            }
+
+            else
+            {
+                Console.WriteLine($"{inputEvenOdd} is odd!");
+            }
+
+        }
+        // Method that checks if a number is postive or negative
+        public static void PosOrNeg()
+        {
+            Console.WriteLine("Write a number! ");
+            int intPosNeg = Convert.ToInt32(Console.ReadLine());
+
+            if (intPosNeg >= 0)
+            {
+                Console.WriteLine($"{intPosNeg} is positive");
+            }
+            
+            else
+            {
+                Console.WriteLine($"{intPosNeg} is negative");
+            }
+
+        }
+
+        // Method that checks if you can vote in the U S A!
+        public static void VotingAge()
+        {
+
+            Console.WriteLine("What is your age? ");
+            int legalAge = Convert.ToInt32(Console.ReadLine());
+
+            if (legalAge >= 18)
+            {
+                Console.WriteLine($"{legalAge} is old enough to vote!");
+            }
+
+            else
+            {
+                Console.WriteLine($"{legalAge} is too young to vote.");
+            }
+
+        }
+        // Method that checks if the user inserted a number from -10 to positive 10!
+        public static void UserRange()
+        {
+            Console.WriteLine("Enter a number! ");
+
+            int range = Convert.ToInt32(Console.ReadLine());
+
+            if (range >= -10 && range < 11)
+            {
+                Console.WriteLine($"{range} is in the range of 10 to negative 10!");
+            }
+
+            else
+            {
+                Console.WriteLine($"{range} is not in the range of 10 to negative 10!");
+            }
+
+
+        }
+
+
+    }
+}
--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -3,18 +3,18 @@
     public class Program
     {
         //LukeWarm Section: Create methods below
-        //Write a method that will print to the console all numbers 1000 through - 1000
+        //Write a method that will print to the console all numbers 1000 through - 1000 - DONE!
 
-        //Write a method that will print to the console numbers 3 through 999 by 3 each time
+        //Write a method that will print to the console numbers 3 through 999 by 3 each time - DONE!
 
-        //Write a method to accept two integers as parameterss and check whether they are equal or not
+        //Write a method to accept two integers as parameterss and check whether they are equal or not - DONE (Kinda!)
         
-        //Write a method to check whether a given number is even or odd
+        //Write a method to check whether a given number is even or odd - DONE!
         
-        //Write a method to check whether a given number is positive or negative
+        //Write a method to check whether a given number is positive or negative - DONE!
         
         //Write a method to read the age of a candidate and determine whether they can vote.
-        //Hint: Use Parse or the safer TryParse() for an extra challenge
+        //Hint: Use Parse or the safer TryParse() for an extra challenge - DONE! (But not with Parse)
         //Parse()
         //TryParse()
 
@@ -27,6 +27,17 @@
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
+            // Main Must DO assignments - Nick Carlson (Uncomment)
+            //Counting.CountingNumbers();
+            //Counting.CountingThrees();
+            //Counting.SameNumber();
+            //Counting.EvenOrOdd();
+            //Counting.PosOrNeg();
+            //Counting.VotingAge();
+
+            //Heatin Up Section
+
+            //Counting.UserRange();
             
         }
     }
